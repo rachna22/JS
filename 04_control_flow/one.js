fd85163dec447ad30929328c65fd5798226a7222
rachna22 <rachnabhattoo45@gmail.com>
2026-07-18 11:49:45 +0530

    js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,39 @@
+// if
+
+/*const isUserLoggedIn = true 
+const temperature = 41 
+
+if( temperature === 40 ){
+    console.log("less than 50");
+} else {
+    console.log("temperature is greater than 50");
+}
+
+console.log("execute") */
+
+// const score = 200 
+
+/* if(score>100){
+        let power = "fly"
+        console.log(` User power : ${power}`) ;
+    }
+
+console.lo(`User power : ${power}`); */
+
+const balance = 1000 
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
+const userLoggedIn = true
+const debitCard = true 
+const loggedInfromGoogle = false 
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2 == 3){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInfromGoogle){
+    console.log("User logged in");
+}
+
